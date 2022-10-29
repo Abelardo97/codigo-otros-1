e87b39124582bb0b57f349e9c2887477f17ff71a
Abelardo Gutierrez Lopez <gabelardo04@gmail.com>
2022-10-28 20:33:54 -0500

    feat: creo que el ejercicio estÃa terminado

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,35 +2,67 @@
  * El programa hace  uso de  api de github para consumir los datos de esta
  *  y nos tiene que mostrar el usuario stolinski la informacion que muestra
  *  será su nombre, su blog y su location
+ * Paso 1: Se modificaron varias cosas algunas fueron eliminadas y reemplazadas
+ * Paso 2: Se modificó la función original y se refactorizo partes del codigo
  */
 
 
-
+//Codigo original
+/*
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-//se agrega . para las class y se arreglaron los nombres de las variables
-const $name = document.querySelector('.name');
-const $blog = document.querySelector('.blog');
-const $location = document.querySelector('.location');
+const $n = document.querySelector('name');
+const $b = document.querySelector('#blog');
+const $l = document.querySelector('.location');
 
-//Se agrega palabra async
-async function displayUser(username) {
-
-  //se cambia el nombre de la variable 
-  $name.textContent = 'cargando...';
+function displayUser(username) {
+  $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(response);
-  
-  //
-  $name.textContent = `${response.name}`;
-  $blog.textContent = `${response.blog}`;
-  $location.textContent = `${response.location}`;
+  console.log(data);
+  $n.textContent = '${data.name}';
+  $b.textContent = '${data.blog}';
+  $l.textContent = '${data.location}';
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  name.textContent = `Algo salió mal: ${err}`
+  n.textContent = `Algo salió mal: ${err}`
 }
 
 displayUser('stolinski').catch(handleError);
+*/
+
+
+
+//Aquí mostraremos los resultados
+ const result = document.querySelector("div#resultado");
+
+//Función para mostrar los datos del usuario
+function atributos({name,blog,location}){
+  result.innerHTML = `
+  <div class="r_text">
+      <p>${name}</p>
+      <p>${blog}</p>
+      <p>${location}</p>
+  </div>`
+}
+
+//Se modificó la funcón original
+function displayUser(username) {
+  const baseEndpoint =  fetch(`https://api.github.com/users/${username}`);
+  baseEndpoint.then(response => response.json())
+    .then((json)=>{
+      atributos(json);
+      console.log(json);
+      return json;
+      
+     
+    })
+    .catch((error) =>{
+      console.log(error);
+    })
+  }    
+
+  //Uso de la funcion
+displayUser('stolinski');
